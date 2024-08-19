006f93e6290ab16ea5e6a36b71c52ac74010cc2f
rohanawan <rohan.awan863@gmail.com>
2024-08-19 05:11:56 +0100

    Updated app.js

--- src/app.js
+++ src/app.js
@@ -17,7 +17,14 @@ const app = express();
 // Parsing
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://ecom-d4ehhbk4x-rohanawans-projects.vercel.app",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+);
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*"); // or specify your frontend origin instead of '*'
     res.header(
